9d1ad587feb0e8f1b66e8ae9cb42dcefce22cf02
Muhammad Bilal <mbilaakbar8@gmail.com>
2026-03-07 00:36:31 +0500

    Page 2 Done

--- script.js
+++ script.js
@@ -37,3 +37,30 @@ nav.addEventListener("mouseleave", function(){
         duration: 0.2,
     })
 })
+
+// Nav Animation Ended
+
+var rightElems = document.querySelectorAll (".right-elem")
+
+rightElems.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        gsap.to(elem.childNodes[3],{
+            opacity: 1,
+            scale: 1
+        })
+        
+    })
+    elem.addEventListener("mouseleave",function(){
+        gsap.to(elem.childNodes[3],{
+            opacity: 0,
+            scale: 0
+
+        })
+    })
+    elem.addEventListener("mousemove",function(dets){
+        gsap.to(elem.childNodes[3],{
+            x:dets.x - elem.getBoundingClientRect().x-90,
+            y:dets.y - elem.getBoundingClientRect().y-220
+        })
+    })
+})
