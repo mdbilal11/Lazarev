89cc1fef03cc875d194b9484fdd85092972b008e
Muhammad Bilal <mbilaakbar8@gmail.com>
2026-03-09 01:25:28 +0500

    Videos Done

--- script.js
+++ script.js
@@ -85,3 +85,19 @@ video.addEventListener("click",function(){
         borderRadius:"30px"
     })
 })
+
+// Video Hover Animation 
+
+var sections = document.querySelectorAll(".sec-right")
+
+sections.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        elem.childNodes[3].style.opacity = 1
+        elem.childNodes[3].play()
+    })
+    elem.addEventListener("mouseleave",function(){
+        elem.childNodes[3].style.opacity = 0
+        elem.childNodes[3].load()
+    })
+
+})
