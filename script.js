84d0ec8b61c5e31237cb331319f7d4af9d9a0932
Muhammad Bilal <mbilaakbar8@gmail.com>
2026-03-12 00:57:04 +0500

    Today work done

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
 function NavAnimation(){
 }
 
-var nav = document.querySelector("nav")
+// var nav = document.querySelector("nav")
 
-nav.addEventListener("mouseenter", function(){
-    let tl = gsap.timeline()
-    tl.to("#nav-bottom", {
-        height: "23vh" 
-    })
-    tl.to(".nav-part2 h5", {
-        display: "block"
-    })
-     tl.to(".nav-part2 h5 span", {
-        y:0,
-        stagger: {
-            amount: 0.1
-        }
-    })
-})
+// nav.addEventListener("mouseenter", function(){
+//     let tl = gsap.timeline()
+//     tl.to("#nav-bottom", {
+//         height: "23vh" 
+//     })
+//     tl.to(".nav-part2 h5", {
+//         display: "block"
+//     })
+//      tl.to(".nav-part2 h5 span", {
+//         y:0,
+//         stagger: {
+//             amount: 0.1
+//         }
+//     })
+// })
 
-nav.addEventListener("mouseleave", function(){
-    let tl = gsap.timeline()
+// nav.addEventListener("mouseleave", function(){
+//     let tl = gsap.timeline()
     
-     tl.to(".nav-part2 h5 span", {
-        y:25,
-        stagger: {
-            amount: 0.1
-        }
-    })
-    tl.to(".nav-part2 h5", {
-        display: "none",
-        duration: 0.1,
-    })
-    tl.to("#nav-bottom", {
-        height: 0,
-        duration: 0.1,
-    })
-})
+//      tl.to(".nav-part2 h5 span", {
+//         y:25,
+//         stagger: {
+//             amount: 0.1
+//         }
+//     })
+//     tl.to(".nav-part2 h5", {
+//         display: "none",
+//         duration: 0.1,
+//     })
+//     tl.to("#nav-bottom", {
+//         height: 0,
+//         duration: 0.1,
+//     })
+// })
 
 // Nav Animation Ended
 
